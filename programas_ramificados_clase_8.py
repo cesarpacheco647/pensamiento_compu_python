96fba6e726986c689bd57f7733ff5789cae733ce
Paul Pacheco <cesarpacheco647@gmail.com>
2020-12-08 20:29:13 -0600

    primer intento

--- programas_ramificados_clase_8.py
+++ programas_ramificados_clase_8.py
@@ -1,12 +1,24 @@
-def players(name, age):
-    name = input("¿Cúal es el nombre del usuario?")
-    age = init(input("¿Qué edad tiene ese usuario?"))
+def players(my_age, bbf_age):
+    if my_age < bbf_age:
+        print("Tú eres más viejo que tu mejor amigo")
+    elif bbf_age > my_age:
+        print("Tu mejor amigo es más viejito que tú")
+    elif my_age == bbf_age:
+        print("tienen la misma edad")
+    else:
+        print("sepa la tostada")
 
 
 def run():
+    print("Conoce quien es más grande, tú o tu mejor amigo.")
+    print("")
+
+    my_age = int(input("¿Cuantos años tienes?"))
+    bbf_age = int(input("¿Cuantos años tiene tu mejor amigo?"))
+
+    result = players(my_age, bbf_age)
+
     
-
-
 
 
 if __name__ == "__main__":
