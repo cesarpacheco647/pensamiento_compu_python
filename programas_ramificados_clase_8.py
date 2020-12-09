5d99f1d6600d803538b6f9ddb3c85ce985d80903
Paul Pacheco <cesarpacheco647@gmail.com>
2020-12-08 22:33:04 -0600

    Lo logré! funciona.

--- programas_ramificados_clase_8.py
+++ programas_ramificados_clase_8.py
@@ -1,10 +1,10 @@
 def players(my_age, bbf_age):
-    if my_age < bbf_age:
-        print("Tú eres más viejo que tu mejor amigo")
-    elif bbf_age > my_age:
-        print("Tu mejor amigo es más viejito que tú")
-    elif my_age == bbf_age:
-        print("tienen la misma edad")
+    if (my_age < bbf_age) == True:
+        print("Tu mejor amigo es más viejito que tú.")
+    elif (my_age > bbf_age) == True:
+        print("Tú eres más viejito que tu mejor amigo.")
+    elif (my_age == bbf_age) == True:
+        print("Tú y tu mejor amigo tienen la misma edad.")
     else:
         print("sepa la tostada")
 
@@ -17,8 +17,6 @@ def run():
     bbf_age = int(input("¿Cuantos años tiene tu mejor amigo?"))
 
     result = players(my_age, bbf_age)
-
-    
 
 
 if __name__ == "__main__":
