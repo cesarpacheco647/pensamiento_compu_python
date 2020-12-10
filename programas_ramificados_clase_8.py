f8f2417ccc436e5f8e65fa3e93d86aabca13af07
Paul Pacheco <cesarpacheco647@gmail.com>
2020-12-10 00:08:16 -0600

    Agregé los nombre de cada uno

--- programas_ramificados_clase_8.py
+++ programas_ramificados_clase_8.py
@@ -1,16 +1,16 @@
-def players(my_age, bbf_age):
-    # función (con parametros)
-    if (my_age < bbf_age) == True:
+def ages (my_name, my_age, bff_name, bff_age):
+    # función (con parametros) 
+    if (my_age < bff_age) == True:
         # si mi edad es menor que la de mi mejor amigo y esto es verdadero
-        print("Tu mejor amigo es más viejito que tú.")
+        print(f"Tu mejor amigo {bff_name} es más viejito que tú.")
         # imprime el mensaje
-    elif (my_age > bbf_age) == True:
+    elif (my_age > bff_age) == True:
         # si mi edad es mayor que la de mi mejor amigo y esto es verdad
-        print("Tú eres más viejito que tu mejor amigo.")
+        print(f"Tú {my_name} eres más viejito que tu mejor amigo.")
         # imprimir mensaje
-    elif (my_age == bbf_age) == True:
+    elif (my_age == bff_age) == True:
         # si mi edad es igual que la de mi mejor amigo y esto es verdad
-        print("Tú y tu mejor amigo tienen la misma edad.")
+        print(f"Tú {my_name} y tu mejor amigo {bff_name} tienen la misma edad.")
         # imprime mensaje
     else:
         print("sepa la tostada")
@@ -23,12 +23,15 @@ def run():
     print("")
     # Impresión en pantalla para bienvenida
 
+    my_name = input("¿Cómo te llamas?")
     my_age = int(input("¿Cuantos años tienes?"))
     # Pregunta al usuario convertida en entero
-    bbf_age = int(input("¿Cuantos años tiene tu mejor amigo?"))
+   
+    bff_name = input("¿Cómo se llama tu mejor amigo?")
+    bff_age = int(input("¿Cuantos años tiene tu mejor amigo?"))
     #Pregunta al usuario convertida a entero
 
-    result = players(my_age, bbf_age)
+    result = ages(my_name, my_age, bff_name, bff_age)
     # resultado de la función (parametros)
 
 
